958c287cf63bf98456c4ccc2d1622cdd82c5dfe7
vickyanushevska <107133031+vickyanushevska@users.noreply.github.com>
2022-06-14 10:35:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2._1_extension
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Country country = new Country();
+            country.main();
+        }
+    }
+}
